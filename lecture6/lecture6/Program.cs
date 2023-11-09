eb8437f404e8da7123947a60d24ed88f65cfc1d0
ocharvat <147035600+ocharvat@users.noreply.github.com>
2023-11-09 09:42:32 +0100

    Add files via upload

---
+++ lecture6/lecture6/Program.cs
@@ -0,0 +1,40 @@
+﻿//void PrintArray(int[] cisla)
+//{
+//    for (int i = 0; i < cisla.Length; i++)
+//    {
+//        Console.WriteLine(cisla[i]);
+//    }
+//}
+
+//int[] cisla = { 1, 2, 3, 4, 5};
+//PrintArray(cisla);
+
+//int[] cisla2 = cisla;
+//PrintArray(cisla);
+//PrintArray(cisla2);
+
+//cisla[0] = 4545;
+//PrintArray(cisla);
+//PrintArray(cisla2);
+
+
+
+
+
+int[] koeficienty  = new int[] {6, 4, 48, -4, 4, -1 };
+
+void PrintPolynomial(int[] koeficienty)
+{
+   int j = koeficienty.Length - 1;
+    
+    for (int i = 0; i < koeficienty.Length; i++) 
+    { 
+      
+       Console.Write(koeficienty[i] + " x^ " + j + " + ");
+
+        j = j - 1;
+      
+    }
+}
+
+PrintPolynomial(koeficienty);
